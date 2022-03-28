10cc74a5d665e985298eba908f9ac7961f20587f
Rashad Gasimzada <helloqwerty00@icloud.com>
2022-03-28 23:14:14 +0400

    homework added

---
+++ FrontToBackEnd/Areas/AdminArea/Controllers/SliderController.cs
@@ -0,0 +1,26 @@
+﻿using FrontToBackEnd.Data;
+using FrontToBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBackEnd.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+            return View(sliders);
+        }
+    }
+}
